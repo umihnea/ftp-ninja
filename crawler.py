3967c9d0a43c121a02eb12bc919353e0227e14c9
OldJackNo7 <ioangaidur@gmail.com>
2017-09-17 17:29:09 +0300

    Added args and commented code

--- crawler.py
+++ crawler.py
@@ -11,18 +11,19 @@ import click
 import sqlite3
 import random
 
-def ftpConn():
+#Connection to the ftp host
+def ftpConn(host):
 	host = 'firl.rsmas.miami.edu'
-	#host = 'ftp.astral.ro'
-	# ftp = ftplib.FTP('69.30.214.178')
 	ftp = ftplib.FTP(host)
 	ftp.login()
 	return ftp
 
+#Connection to the sqlite3 db
 def dbConn():
 	conn = sqlite3.connect('files.db')
 	return conn
 
+#Inserting into db all files, commiting at random intervals
 def indexerDB(server, filename, path, conn):
 	randomNum = random.randint(1, 32)
 	if randomNum == 16:
@@ -31,7 +32,9 @@ def indexerDB(server, filename, path, conn):
 	c = conn.cursor()
 	c.execute("INSERT INTO FILES (SERVER, NAME, PATHTO) VALUES (?, ?, ?)",(server, filename, path))
 
+#Checks if it's a file or a folder by verifying if it has size
 def isFile(ftp, filename):
+	#TYPE I is only for some ftp servers which don't support ASCII encoding
 	ftp.voidcmd('TYPE I')
 	try:
 		ftp.size(filename)
@@ -39,20 +42,22 @@ def isFile(ftp, filename):
 	except:
 		return False
 
+#Main code here, it's recursive and it crawls all the folders in a path
 def crawlFtp(ftp, path, conn):
+	#Lines stores all the data from ftp.dir()
 	lines = []
 	try:
 		ftp.cwd(str(path))
 		ftp.dir(str(path), lines.append)
-		results = []
+		#Folders stores all the folders for the recursive call
 		folders = []
 		for line in lines:
 			fields = line.split()
+			#Combining fileds for the files/folders that are using spaces in their name
 			name = ' '.join(fields[8:])
 			if isFile(ftp, name) == True:
 				indexerDB(ftp.host, name, str(path), conn)
 				logging.debug("File " + name)
-
 			else:
 				name = name.replace("/", "")
 				folders.append(name)
@@ -60,25 +65,39 @@ def crawlFtp(ftp, path, conn):
 
 		for folder in folders:
 			logging.debug("Entering folder \t" + folder)
-			#crawlFtp(ftp, Path(path, folder), conn)
+			crawlFtp(ftp, Path(path, folder), conn)
 
 	except Exception as e:
 		logging.error(e, exc_info=True)
 
-def main():
-	path = Path('/users/lfiorentino/Books/')
-	conn = dbConn()
+@click.command()
+@click.option('--host', prompt='Host', help='FTP server host ip/address')
+@click.option('--path', prompt='Path', default='/', help='Enter the path to index')
+def worker(path, host):
+	#Clearing log file
 	with open('log.log', 'w'):
 		pass
+
+	#Initializing log
 	logging.basicConfig(filename='log.log',level=logging.DEBUG, format="%(levelname)s|%(asctime)s|%(message)s")
 	logging.info("Starting crawling")
-	ftp = ftpConn()
+
+	#Connecting to the ftp server
+	ftp = ftpConn(host)
+
+	#Connecting to the db
+	conn = dbConn()
+
+	#Initializing crawler
 	crawlFtp(ftp, path, conn)
+
+	#Last commit to the db
 	conn.commit()
 	logging.info("Last commit to db")
 	logging.info("Job done!")
+
+	#Killing the connection to the ftp
 	ftp.quit()
 
-
-
-main()
+if __name__ == '__main__':
+    worker()
